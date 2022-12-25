8123a50f45ee7d046807f83cbed4ddd68e3f0177
sriarjun <srimanikantaarjun@gmail.com>
2022-12-24 20:55:19 -0500

    Update 1 Two Sum.py

--- December/Dec 24/1 Two Sum.py
+++ December/Dec 24/1 Two Sum.py
@@ -1,1 +1,32 @@
+"""
+https://leetcode.com/problems/two-sum/description/
 
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+"""
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hmap = {}
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in hmap:
+                return [hmap[diff], i]
+            hmap[nums[i]] = i
+        return
+
+
+# A more pythonic solution from neetcode.io
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevMap = {}  # val -> index
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i

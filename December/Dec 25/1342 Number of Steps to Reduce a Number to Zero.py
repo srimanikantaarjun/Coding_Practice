8246f7ace11f0b2c7930ad9088d9583c79e1dbf1
sriarjun <srimanikantaarjun@gmail.com>
2022-12-25 18:48:28 -0500

    Create 1342 Number of Steps to Reduce a Number to Zero.py

---
+++ December/Dec 25/1342 Number of Steps to Reduce a Number to Zero.py
@@ -0,0 +1,7 @@
+"""
+https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/description/
+
+Given an integer num, return the number of steps to reduce it to zero.
+In one step, if the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
+"""
+

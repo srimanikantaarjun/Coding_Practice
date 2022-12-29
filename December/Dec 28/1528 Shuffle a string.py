c30fb94631f596d7bb752b81f4b84002333b363e
sriarjun <srimanikantaarjun@gmail.com>
2022-12-28 20:24:20 -0500

    Create 1528 Shuffle a string.py

---
+++ December/Dec 28/1528 Shuffle a string.py
@@ -0,0 +1,14 @@
+"""
+https://leetcode.com/problems/shuffle-string/description/
+You are given a string s and an integer array indices of the same length.
+The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
+Return the shuffled string.
+"""
+
+
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        res = [''] * len(s)
+        for index, char in enumerate(s):
+            res[indices[index]] = char
+        return "".join(res)

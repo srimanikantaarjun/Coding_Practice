cb9ceabc74bbfa32850233f0ed1f206c49ba0a65
sriarjun <srimanikantaarjun@gmail.com>
2022-12-31 19:56:41 -0500

    Create 278 First Bad Version.py

---
+++ December/Dec 31/278 First Bad Version.py
@@ -0,0 +1,27 @@
+"""
+https://leetcode.com/problems/first-bad-version/?envType=study-plan&id=algorithm-i
+You are a product manager and currently leading a team to develop a new product.
+Unfortunately, the latest version of your product fails the quality check.
+Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+You are given an API bool isBadVersion(version) which returns whether version is bad.
+Implement a function to find the first bad version. You should minimize the number of calls to the API.
+"""
+
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        l = 1
+        r = n
+        while l < r:
+            mid = (l + r)//2
+            res = isBadVersion(mid)
+            if not res:
+                l = mid + 1
+            elif res:
+                r = mid
+        return l

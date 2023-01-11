bb6ae3fa4c83ae23382790e04ce1ffa2315fe361
sriarjun <srimanikantaarjun@gmail.com>
2023-01-10 21:20:48 -0500

    Create 144 Binary Tree Preorder Traversal.py

---
+++ January 2023/January 2023 2nd week/January 10/144 Binary Tree Preorder Traversal.py
@@ -0,0 +1,22 @@
+"""
+https://leetcode.com/problems/binary-tree-preorder-traversal/
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        def dfs(root):
+            if not root:
+                return
+            res.append(root.val)
+            dfs(root.left)
+            dfs(root.right)
+        dfs(root)
+        return res

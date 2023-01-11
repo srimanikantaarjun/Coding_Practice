46625d3fca74b8cd73a53499ac38b5b76de8f458
sriarjun <srimanikantaarjun@gmail.com>
2023-01-10 21:22:51 -0500

    Create 94 Binary Tree Inorder Traversal.py

---
+++ January 2023/January 2023 2nd week/January 10/94 Binary Tree Inorder Traversal.py
@@ -0,0 +1,22 @@
+"""
+https://leetcode.com/problems/binary-tree-inorder-traversal/
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        def dfs(root):
+            if not root:
+                return
+            dfs(root.left)
+            res.append(root.val)
+            dfs(root.right)
+        dfs(root)
+        return res

aefbe37381206a4e3a13cbca46f275ad276141b9
sriarjun <srimanikantaarjun@gmail.com>
2023-01-10 21:25:58 -0500

    Create 230 Kth Smallest Element in a BST.py

---
+++ January 2023/January 2023 2nd week/January 10/230 Kth Smallest Element in a BST.py
@@ -0,0 +1,24 @@
+"""
+https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/
+Given the root of a binary search tree, and an integer k,
+return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        stack = []
+        while k:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            k -= 1
+            if not k:
+                return root.val
+            root = root.right

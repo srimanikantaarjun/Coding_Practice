a721065b0066c51d0681443bae861c47eedc4aaa
sriarjun <srimanikantaarjun@gmail.com>
2023-01-15 18:53:59 -0500

    Update 107 Binary Tree Level Order Traversal II.py

--- January 2023/January 2023 2nd week/January 15/107 Binary Tree Level Order Traversal II.py
+++ January 2023/January 2023 2nd week/January 15/107 Binary Tree Level Order Traversal II.py
@@ -4,3 +4,29 @@ https://leetcode.com/problems/binary-tree-level-order-traversal-ii/
 Given the root of a binary tree, return the bottom-up level order traversal of its nodes' values.
 (i.e., from left to right, level by level from leaf to root).
 """
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = []
+        if root is None:
+            return res
+        dq = deque()
+        dq.append(root)
+        while dq:
+            size = len(dq)
+            temp = []
+            for i in range(size):
+                node = dq.popleft()
+                temp.append(node.val)
+                if node.left is not None:
+                    dq.append(node.left)
+                if node.right is not None:
+                    dq.append(node.right)
+            res.append(temp)
+        return res[::-1]

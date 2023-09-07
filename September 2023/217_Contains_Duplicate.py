42bbf048073d0cd72b212b44a6c6649061d657f9
sriarjun <srimanikantaarjun@gmail.com>
2023-09-07 14:47:43 -0400

    Update 217_Contains_Duplicate.py

--- September 2023/217_Contains_Duplicate.py
+++ September 2023/217_Contains_Duplicate.py
@@ -1,1 +1,10 @@
 # https://leetcode.com/problems/contains-duplicate/
+
+def containsDuplicate(nums):
+  hset = set()
+  for ele in nums:
+    if ele in hset:
+      return True
+    else:
+      hset.add(ele)
+  return False
